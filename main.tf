4db59248773f6896c4f2c7541eb384246619f44d
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:26:55 +0000

    Update to schema version v4.57.0

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_aadb2c_directory" "aadb2c_directorys" {
+  for_each = var.aadb2c_directorys
+
+  data_residency_location = each.value.data_residency_location
+  domain_name             = each.value.domain_name
+  resource_group_name     = each.value.resource_group_name
+  sku_name                = each.value.sku_name
+  country_code            = each.value.country_code
+  display_name            = each.value.display_name
+  tags                    = each.value.tags
+}
+
---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "aadb2c_directorys" {
+  description = "All aadb2c_directory resources"
+  value       = azurerm_aadb2c_directory.aadb2c_directorys
+}
+
